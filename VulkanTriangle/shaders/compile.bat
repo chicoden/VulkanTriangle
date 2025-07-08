91d4ec298885370c89ef075a65016ce737ecc475
Elijah Bevers <elijah.bevers@oit.edu>
2025-07-07 18:47:56 -0700

    Specified profile in shader compilation script

--- VulkanTriangle/shaders/compile.bat
+++ VulkanTriangle/shaders/compile.bat
@@ -1,2 +1,2 @@
-C:/slang/slang-2025.12-windows-x86_64/bin/slangc.exe src/simple.slang -target spirv -o bin/simple.spv
+C:/slang/slang-2025.12-windows-x86_64/bin/slangc.exe src/simple.slang -profile glsl_450 -target spirv -o bin/simple.spv
 pause
